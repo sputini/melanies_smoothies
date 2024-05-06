11b5b412b0a95398ae159c47be008832bb3eca2c
Sandro Putini <sandro.putini@gmail.com>
2024-05-06 15:50:18 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,6 @@ if ingredients_list:
 # New section to display fruityvice nutrition information
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=true)
+fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
 
 
